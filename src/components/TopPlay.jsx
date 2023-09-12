c168a3c9ad118a09962e405a3d4c509869fac9ca
Jhymer Martínez <jhmartinez1991@gmail.com>
2023-09-11 19:20:45 -0500

    feat: display top artist

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -11,9 +11,30 @@ import { useGetTopChartQuery } from '../redux/services/deezer';
 import 'swiper/css';
 import 'swiper/css/free-mode';
 
-const TopChartCard = ({ song, idx }) => (
-  <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
-    {song?.title}
+const TopChartCard = ({ song, idx, isPlaying, activeSong, handlePauseClick, handlePlayClick }) => (
+  <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg mb-2">
+    <h3 className="font-bold text-base text-white mr-3">
+      {idx + 1}.
+    </h3>
+    <div className="flex-1 flex flex-row justify-between items-center">
+      <img src={song?.album?.cover_medium} alt={song?.title} className="w-20 h-20 rounded-lg" />
+      <div className="flex-1 flex flex-col justify-center mx-3">
+        <Link to={`/songs/${song?.id}`}>
+          <p className="text-xl font-bold text-white">{song?.title}</p>
+        </Link>
+        <Link to={`/artists/${song?.artist?.id}`}>
+          <p className="text-base text-gray-300 mt-1">{song?.artist?.name}</p>
+        </Link>
+      </div>
+    </div>
+
+    <PlayPause
+      isPlaying={isPlaying}
+      activeSong={activeSong}
+      song={song}
+      handlePause={handlePauseClick}
+      handlePlay={handlePlayClick}
+    />
   </div>
 );
 
@@ -28,14 +49,14 @@ const TopPlay = () => {
     divRef.current.scrollIntoView({ behavior: 'smooth' });
   });
 
-  // const handlePauseClick = () => {
-  //   dispatch(playPause(false));
-  // };
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
 
-  // const handlePlayClick = () => {
-  //   dispatch(setActiveSong({ song, data, i }));
-  //   dispatch(playPause(true));
-  // };
+  const handlePlayClick = (song, i) => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  };
 
   return (
     <div ref={divRef} className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col">
@@ -46,7 +67,15 @@ const TopPlay = () => {
         </div>
         <div className="mt-4 flex flex-col gap-1">
           {topPlays?.map((song, idx) => (
-            <TopChartCard key={song.id} song={song} idx={idx} />
+            <TopChartCard
+              key={song.id}
+              song={song}
+              idx={idx}
+              isPlaying={isPlaying}
+              activeSong={activeSong}
+              handlePauseClick={handlePauseClick}
+              handlePlayClick={() => handlePlayClick(song, idx)}
+            />
           ))}
         </div>
       </div>
