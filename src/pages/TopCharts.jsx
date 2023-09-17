37260439570054a5f31f55c50894f68b2f5ef935
Jhymer Martínez <jhmartinez1991@gmail.com>
2023-09-17 08:28:42 -0500

    feat: add the Top Songs page

--- src/pages/TopCharts.jsx
+++ src/pages/TopCharts.jsx
@@ -1,3 +1,34 @@
-const TopCharts = () => <div>TopCharts</div>;
+import { useSelector } from 'react-redux';
+import { useGetTopSongsByGenreQuery } from '../redux/services/deezer';
+import { Error, Loader, SongCard } from '../components';
+
+const TopCharts = () => {
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data, isFetching, error } = useGetTopSongsByGenreQuery(); // All genres
+
+  if (isFetching) return <Loader title="Loading songs..." />;
+  if (error) return <Error title="Oops! Something went wrong." />;
+
+  return (
+    <div className="flex flex-col">
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+        Discover Top Songs
+      </h2>
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data.map((song, idx) => (
+          <SongCard
+            key={song.id}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+            i={idx}
+          />
+        ))}
+      </div>
+
+    </div>
+  );
+};
 
 export default TopCharts;
