4b5414943c5bc037eb1b23389da1ad8b7dc82c7f
Jhymer Martínez <jhmartinez1991@gmail.com>
2023-09-20 08:15:21 -0500

    feat: rename search bar

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,25 +1,36 @@
+import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
+import { useNavigate } from 'react-router-dom';
 
-const Searchbar = () => (
-  <form autoComplete="off" className="p-2 text-gray-400 focus-within:text-gray-600">
-    <label htmlFor="search-field" className="sr-only">
-      Search all songs
-    </label>
-    <div className="flex flex-row justify-start items-center">
-      <FiSearch className="w-5 h-5 ml-4" />
-      <input
-        type="search"
-        name="search-field"
-        autoComplete="off"
-        id="search-field"
-        placeholder="Search"
-        value=""
-        onChange={() => {}}
-        className="flex-1 bg-transparent border-none outline-none placeholder-gray-500 text-base text-white p-4"
-      />
-    </div>
+const SearchBar = () => {
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState('');
 
-  </form>
-);
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+    navigate(`/search/${searchTerm}`);
+  };
 
-export default Searchbar;
+  return (
+    <form onSubmit={handleSubmit} autoComplete="off" className="p-2 text-gray-400 focus-within:text-gray-600">
+      <label htmlFor="search-field" className="sr-only">
+        Search all songs
+      </label>
+      <div className="flex flex-row justify-start items-center">
+        <FiSearch className="w-5 h-5 ml-4" />
+        <input
+          type="search"
+          name="search-field"
+          autoComplete="off"
+          id="search-field"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={(ev) => setSearchTerm(ev.target.value)}
+          className="flex-1 bg-transparent border-none outline-none placeholder-gray-500 text-base text-white p-4"
+        />
+      </div>
+    </form>
+  );
+};
+
+export default SearchBar;
